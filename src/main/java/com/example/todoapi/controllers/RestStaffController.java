980afc371ccd4944186df717cd7c2d4a30c3ad0e
quyen <nguyenquyen5120@gmail.com>
2022-05-18 09:14:23 +0700

    change

---
+++ src/main/java/com/example/todoapi/controllers/RestStaffController.java
@@ -0,0 +1,11 @@
+package com.example.todoapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/staff")
+public class RestStaffController {
+
+
+}

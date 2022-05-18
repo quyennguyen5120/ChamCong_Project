0d821d16ebbd65876f06a3c03ac8e2649d40a64d
quyen <nguyenquyen5120@gmail.com>
2022-05-18 09:19:57 +0700

    change

---
+++ src/main/java/com/example/todoapi/entities/Timekeeping.java
@@ -0,0 +1,31 @@
+package com.example.todoapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "timekeeping")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Timekeeping {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "time_start")
+    private Timestamp timeStart;
+
+    @Column(name = "end_start")
+    private Timestamp endStart;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    private StaffEntity staff;
+
+}

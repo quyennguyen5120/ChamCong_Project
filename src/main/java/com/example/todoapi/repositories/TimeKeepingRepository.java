d3642c1fc523c0b5b83ad2f5e04461e7522273a8
dangnh-dev <haidang14727@gmail.com>
2022-05-19 15:45:45 +0700

    .

--- src/main/java/com/example/todoapi/repositories/TimeKeepingRepository.java
+++ src/main/java/com/example/todoapi/repositories/TimeKeepingRepository.java
@@ -17,5 +17,5 @@ public interface TimeKeepingRepository extends JpaRepository<Timekeeping, Long> 
     @Query("select new com.example.todoapi.dtos.TimekeepingDTO(t) from Timekeeping t where t.staff.id = ?1")
     public List<TimekeepingDTO> findByStaffId(Long staffId);
 
-
+    public List<Timekeeping> findAllByTimeStartBetween();
 }
